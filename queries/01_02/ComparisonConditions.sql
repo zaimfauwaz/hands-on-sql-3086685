ef1ca3bdd1bc5b621de97f46772cb1c2a4761864
MatEdith <101429920+zaimfauwaz@users.noreply.github.com>
2023-09-21 03:07:10 +0000

    Edited for my copy: Testing on ComparisonConditions

--- queries/01_02/ComparisonConditions.sql
+++ queries/01_02/ComparisonConditions.sql
@@ -1,9 +1,9 @@
 SELECT empno, name,hiredate 
 FROM emp_tab 
-WHERE name LIKE '_D%'; 
+WHERE name LIKE '_I%'; 
 
 SELECT * FROM emp_tab 
-WHERE deptno IN (30,40);
+WHERE deptno IN (30,40,70);
 
 SELECT * FROM emp_tab 
 WHERE salary BETWEEN 2000 AND 6000;
